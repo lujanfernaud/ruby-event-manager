826aaf2e2dfdf468bfd12e75ddb5584c0d0cd898
lujanfernaud <lujanfernaud@gmail.com>
2017-04-11 21:19:03 +0100

    Add project comment and start implementation

---
+++ lib/event_manager.rb
@@ -0,0 +1,8 @@
+# Project: Event Manager in Ruby for The Odin Project
+# Author: Luján Fernaud
+# URL: http://tutorials.jumpstartlab.com/projects/eventmanager.html
+
+puts "Event Manager initialized."
+
+contents = File.read("event_attendees.csv")
+puts contents
